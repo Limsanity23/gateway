9a32defdfedc2a53b4cca108a89902b1209143b2
jinyounglee <hs25823@naver.com>
2020-07-14 17:47:09 +0900

    주차시간 위반 조회 쿼리 수정

--- src/main/java/com/bnids/gateway/repository/VisitCarRepositorySupport.java
+++ src/main/java/com/bnids/gateway/repository/VisitCarRepositorySupport.java
@@ -49,7 +49,7 @@ public class VisitCarRepositorySupport extends QuerydslRepositorySupport {
             builder.and(visitCar.entvhclDt.after(LocalDateTime.now().minusDays(dto.getRegistDay())));
         }
         if( dto.getWarinigCarRulesSection().equals(WarningCarRegistEnum.PARKING_DURATION_VIOLATION) ) {
-            builder.and(Expressions.numberTemplate(Long.class, "TIMEDIFF({0}, {1})", visitCar.lvvhclDt, visitCar.entvhclDt).gt(dto.getParkingTime()));
+            builder.and(Expressions.numberTemplate(Long.class, "timestampdiff(HOUR, {0}, {1})", visitCar.entvhclDt , visitCar.lvvhclDt).gt(dto.getParkingTime()));
         }
         return builder;
     }
