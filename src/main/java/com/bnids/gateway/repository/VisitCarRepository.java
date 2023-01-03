fcb30e59694cbdb7dc3484fe8a10c98acfda5cde
sooki <purplerenivie@gmail.com>
2023-01-03 11:17:27 +0900

    출차찾기 쿼리 로직개선 > 입출차 시간이 같거나 출차가 널인 경우로 수정 및 마지막 입차데이터 찾기 개선

--- src/main/java/com/bnids/gateway/repository/VisitCarRepository.java
+++ src/main/java/com/bnids/gateway/repository/VisitCarRepository.java
@@ -10,7 +10,19 @@ import java.util.Optional;
 
 public interface VisitCarRepository extends BaseJPARepository<VisitCar, Long> {
     Optional<VisitCar> findById(Long id);
+
+    @Query(value = "SELECT visit_car_id, created_dt, site_code, updated_dt, car_no,\n" +
+            "       car_section, entrance_gate_id, entrance_gate_status, entrance_gate_transit_mode,\n" +
+            "       entrance_worker, entvhcl_dt, exit_gate_id, exit_gate_status, exit_gate_transit_mode,\n" +
+            "       exit_worker, logic_code, lpr_car_no, lvvhcl_dt, note, restrict_leave_car,\n" +
+            "       tel_no, visit_allowable_time, visit_dong, visit_ho, visit_place_name, visit_purpose \n" +
+            "FROM   visit_car \n" +
+            "WHERE  visit_car_id IN ( \n" +
+            "               SELECT max(visit_car_id) FROM visit_car WHERE  car_no = :carNo \n" +
+            "               AND (entvhcl_dt = lvvhcl_dt OR lvvhcl_dt IS NULL ) \n" +
+            ")", nativeQuery = true) //221214 cks 본 쿼리메서드를  네이티브로 변경함 - 입출차가 같은 row가 조건에 추가 됨
     Optional<VisitCar> findTopByCarNoAndLvvhclDtIsNullOrderByEntvhclDtDesc(String carNo);
+
     Optional<VisitCar> findTopByCarNoOrderByEntvhclDtDesc(String carNo);
 
     @Query(value = 
@@ -27,4 +39,5 @@ public interface VisitCarRepository extends BaseJPARepository<VisitCar, Long> {
     , nativeQuery=true)
     // car_section 4 or 5의 입차일 혹은 출차일이 월초부터 현재까지인 해당 동호 방문 차량의 주차시간을 합산
     Double getSumVisitCar45ParkingHours(@Param("visit_dong") String visit_dong, @Param("visit_ho") String visit_ho);
+
 }
